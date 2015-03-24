4ef735b111dc1118e3e6a5d507aec785fc6cced5
Allister Banks <arubdesu@me.com>
2015-03-24 14:58:15 -0400

    better handling of nopkg-infos and icons files with spaces

--- moscargo.py
+++ moscargo.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask import render_template, redirect
+from flask import render_template, redirect#jsonify
 app = Flask(__name__)
 from operator import itemgetter
 import plistlib
 import os
 
-#set this to your munki repo's 'all' catalog path
-catalog_to_parse = '/Users/Shared/repo/catalogs/all'
+#set this to the base of your munki repo:
+repo_base = '/Users/Shared/repo/'
+# ad override the catalog to parse below:
+catalog_to_parse = 'all'
 
 # yup, stolen whole-heartedly from http://stackoverflow.com/a/22878816/743638
 def get_key_watcher():
@@ -21,28 +23,28 @@ def get_key_watcher():
     return key_not_seen
 
 try:
-    products = plistlib.readPlist(catalog_to_parse)
+    products = plistlib.readPlist(repo_base + 'catalogs/all')
     prodlist = []
-    name_vers_dict = {}
     for prod_dict in products:
         if not prod_dict.get('installer_type') == 'apple_update_metadata':
-            joined_path = ('static/icons/' + prod_dict.get('name') + '.png')
-            this_prod_dict = {
-                            'Name': prod_dict.get('display_name'),
-                            'distinct_name': prod_dict.get('name'),
-                            'description': (prod_dict.get('description'))[:130] + '...',
-                            'link': ('static/pkgs/' + prod_dict.get('installer_item_location')).replace(' ', '%20'),
-                            'version': prod_dict.get('version'),
-                            }
-            if prod_dict.get('installer_type') == 'profile':
-                this_prod_dict['icon_url'] = 'static/mobileconfig.png'
-            elif prod_dict.get('icon_name'):
-                this_prod_dict['icon_url'] = os.path.join('static/icons/', prod_dict.get('icon_name'))
-            elif os.path.exists(joined_path):
-                this_prod_dict['icon_url'] = joined_path.replace(' ', '%20')
-            else:
-                this_prod_dict['icon_url'] = 'static/package.png'
-            prodlist.append(this_prod_dict)
+            if not prod_dict.get('installer_type') == 'nopkg':
+                joined_path = (repo_base + 'icons/' + prod_dict.get('name') + '.png')
+                this_prod_dict = {
+                                'Name': prod_dict.get('display_name'),
+                                'distinct_name': prod_dict.get('name'),
+                                'description': (prod_dict.get('description'))[:130] + '...',
+                                'link': ('static/pkgs/' + prod_dict.get('installer_item_location')).replace(' ', '%20'),
+                                'version': prod_dict.get('version'),
+                                }
+                if prod_dict.get('installer_type') == 'profile':
+                    this_prod_dict['icon_url'] = 'static/mobileconfig.png'
+                elif prod_dict.get('icon_name'):
+                    this_prod_dict['icon_url'] = 'static/icons/' + prod_dict.get('icon_name').replace(' ', '%20')
+                elif os.path.exists(joined_path):
+                    this_prod_dict['icon_url'] = 'static/icons/' + (prod_dict.get('name') + '.png').replace(' ', '%20')
+                else:
+                    this_prod_dict['icon_url'] = 'static/package.png'
+                prodlist.append(this_prod_dict)
     listbyvers = sorted(prodlist, key=itemgetter('version'), reverse=True)
     filtered = filter(get_key_watcher(), listbyvers)
     sprodlist = sorted(filtered, key=itemgetter('Name'))
